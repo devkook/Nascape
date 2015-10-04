8022b370471705b0e87f7503118eda2d8b576373
min.kyoungkook <min.kyoungkook@cashslide.co.kr>
2015-10-05 00:13:32 +0900

    선상님 코드 복붙

--- app/src/main/java/com/diginori/nascape/ActMain.java
+++ app/src/main/java/com/diginori/nascape/ActMain.java
@@ -1,7 +1,50 @@
 package com.diginori.nascape;
 
-/**
- * Created by diginori on 15. 10. 4..
- */
-public class ActMain {
+import android.app.Activity;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class ActMain extends Activity {
+    private Context mContext;
+    private WebView mWebView;
+    private EditText mEtAddress;
+    private Button mBtnGo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_main);
+
+        this.mContext = this;
+
+        mWebView = (WebView) findViewById(R.id.wv_act_main);
+        mWebView.setWebViewClient(new WebViewClient());
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+
+        mEtAddress = (EditText) findViewById(R.id.et_address_act_main);
+
+        mBtnGo = (Button) findViewById(R.id.btn_go_act_main);
+        mBtnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String urlStr = mEtAddress.getText().toString();
+
+                if (!urlStr.startsWith("http://")) {
+                    urlStr = "http://" + urlStr;
+                }
+                mWebView.loadUrl(urlStr);
+            }
+        });
+    }
 }
